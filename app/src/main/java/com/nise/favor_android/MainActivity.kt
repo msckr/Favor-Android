9a1189e74c37c0bb3f673600495bbc0bbd6b662e
Minsoo Jo <mschoyb@naver.com>
2023-01-18 19:20:42 +0900

    Feat. splash 수정

--- app/src/main/java/com/nise/favor_android/MainActivity.kt
+++ app/src/main/java/com/nise/favor_android/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.nise.favor_android
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.nise.favor_android.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        register_btn.setOnClickListener {
+            val intent = Intent(this,Register::class.java)
+            startActivity(intent)
+        }
     }
 }
