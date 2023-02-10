02d360fdcfb3a42eb92399a2e10cf04269093e1e
Minsoo Jo <mschoyb@naver.com>
2023-02-10 18:48:43 +0900

    Feat. 검색 레이아웃 추가

---
+++ app/src/main/java/com/nise/favor_android/Home/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.nise.favor_android.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nise.favor_android.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
