558544e6937d05a99a789b1d2c5e96ab98941e7f
Minsoo Jo <mschoyb@naver.com>
2023-03-23 22:03:04 +0900

    Add. response UserDTO 추가

---
+++ app/src/main/java/com/nise/favor_android/Login/Data.kt
@@ -0,0 +1,13 @@
+package com.nise.favor_android.Login
+
+data class Data(
+    val email: String,
+    val favorList: List<Any>,
+    val friendList: List<Any>,
+    val giftlist: List<Any>,
+    val name: Any,
+    val reminderList: List<Any>,
+    val role: String,
+    val userNo: Int,
+    val userid: Any
+)
---
+++ app/src/main/java/com/nise/favor_android/Login/UserDTO.kt
@@ -0,0 +1,7 @@
+package com.nise.favor_android.Login
+
+data class UserDTO(
+    val `data`: Data,
+    val responseCode: String,
+    val responseMessage: String
+)
