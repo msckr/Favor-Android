a5638b1c679f8f6578a77f9e634a522ad4e86550
Minsoo Jo <mschoyb@naver.com>
2023-04-01 12:57:36 +0900

    Add 선물추가 레이아웃 구성

--- app/src/main/java/com/nise/favor_android/Home/Add/GiftRegisterActivity.kt
+++ app/src/main/java/com/nise/favor_android/Home/Add/GiftRegisterActivity.kt
@@ -2,147 +2,49 @@
 
 package com.nise.favor_android.Home.Add
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.compose.setContent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.*
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.*
-import com.google.android.material.tabs.TabLayout
-import kotlinx.coroutines.launch
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import com.nise.favor_android.Home.Add.ui.GivePrFragment
+import com.nise.favor_android.Home.Add.ui.RecievePrFragment
+import com.nise.favor_android.Home.Add.ui.RegisterAdapter
+import com.nise.favor_android.databinding.ActivityGiftRegisterBinding
 
 
 class GiftRegisterActivity : AppCompatActivity() {
-    @OptIn(ExperimentalPagerApi::class)
+    private lateinit var binding: ActivityGiftRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-
-                TabLayout()
-        }
+        binding = ActivityGiftRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViewPager()
+        binding.register.isUserInputEnabled=false
     }
-}
-@Preview
-@ExperimentalPagerApi
-@Composable
-fun TabLayout() {
+    private fun initViewPager() {
+        var viewPager2Adatper = RegisterAdapter(this)
+        viewPager2Adatper.addFragment(RecievePrFragment())
+        viewPager2Adatper.addFragment(GivePrFragment())
 
-    val pagerState = rememberPagerState( 2)
+        binding.register.apply {
+            adapter = viewPager2Adatper
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
 
-    Column(
-        modifier = Modifier.background(White)
-    ) {
-        TopAppBar(backgroundColor = White) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "Tab Layout Example",
-                    style = TextStyle(color = Black),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = TextUnit(
-                        18F,
-                        TextUnitType.Sp
-                    ),
-                    modifier = Modifier.padding(all = Dp(5F)),
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-        Tabs(pagerState = pagerState)
-        TabsContent(pagerState = pagerState)
-    }
-}
-
-@ExperimentalPagerApi
-@Composable
-fun Tabs(pagerState: PagerState) {
-    val list = listOf(
-        "Home" to Icons.Default.Home,
-        "Shopping" to Icons.Default.ShoppingCart
-    )
-    val scope = rememberCoroutineScope()
-    TabRow(
-        selectedTabIndex = pagerState.currentPage,
-
-        backgroundColor = White,
-
-        contentColor = Black,
-
-        indicator = { tabPositions ->
-            TabRowDefaults.Indicator(
-                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
-                height = 2.dp,
-                color = Black
-            )
-        }
-    ) {
-        list.forEachIndexed { index, _ ->
-            Tab(
-                icon = {
-                    Icon(imageVector = list[index].second, contentDescription = null)
-                },
-                text = {
-                    Text(
-                        list[index].first,
-                        color = if (pagerState.currentPage == index) Black else Color.LightGray
-                    )
-                },
-                selected = pagerState.currentPage == index,
-                onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
+                    super.onPageSelected(position)
                 }
-            )
+            })
         }
-    }
-}
 
+        TabLayoutMediator(binding.registerTab, binding.register) { tab, position ->
+            Log.e("YMC", "ViewPager position: ${position}")
+            when (position) {
+                0 -> tab.text = "준 선물"
+                1 -> tab.text = "받은 선물"
+            }
+        }.attach()
+    }
 
-@ExperimentalPagerApi
-@Composable
-fun TabsContent(pagerState: PagerState) =
-    HorizontalPager(state = pagerState) {
-            page ->
-        when (page) {
-            0 -> TabContentScreen(data = "Welcome to Home Screen")
-            1 -> TabContentScreen(data = "Welcome to Shopping Screen")
-        }
-    }
-@Composable
-fun TabContentScreen(data: String) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = data,
-            style = MaterialTheme.typography.h5,
-            color = Black,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
-    }
 }
