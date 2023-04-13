10a201ce49ee7d32e948fe9ed0c32de3ef62a57e
YangNu <bin450579@gmail.com>
2023-04-13 22:59:59 +0900

    [Fix] TimelineitemAdapter bind 방식 수정

--- app/src/main/java/com/nise/favor_android/Home/ui/itemUI/TimelineitemAdapter.kt
+++ app/src/main/java/com/nise/favor_android/Home/ui/itemUI/TimelineitemAdapter.kt
@@ -6,24 +6,32 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.nise.favor_android.R
+import com.nise.favor_android.databinding.CardviewTimelineBinding
 
-class TimelineitemAdapter():RecyclerView.Adapter<TimelineitemAdapter.TimelineitemViewHolder>() {
-    var images = intArrayOf(R.drawable.favor_icon,
-                            R.drawable.favor_icon,
-                            R.drawable.favor_icon,
-                            R.drawable.favor_icon,)
+class TimelineitemAdapter : RecyclerView.Adapter<TimelineitemAdapter.TimelineitemViewHolder>() {
+    var images = arrayListOf<Int>()
 
-    class TimelineitemViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview){
+    private lateinit var binding: CardviewTimelineBinding
+
+    /*class TimelineitemViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
         val photoimage: ImageView = itemview.findViewById(R.id.photo_image)
-    }
+        fun bind()
+    }*/
 
+    inner class TimelineitemViewHolder(binding: CardviewTimelineBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Int) {
+            binding.photoImage.setImageResource(data)
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimelineitemViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_timeline,parent,false)
-        return TimelineitemViewHolder(view)
+        binding = CardviewTimelineBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        //var view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_timeline, parent, false)
+        return TimelineitemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TimelineitemViewHolder, position: Int) {
-        holder.photoimage.setImageResource(images.get(position))
+        //holder.photoimage.setImageResource(images.get(position))
+        holder.bind(images[position])
     }
 
     override fun getItemCount(): Int {
